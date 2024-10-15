ea9cf5cec81d3187b36d8dac1c778e3ba9048bc7
Negaram71 <146989928+Negaram71@users.noreply.github.com>
2024-10-14 21:48:40 -0500

    Add files via upload

---
+++ hey_day.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Hey Day Bash Script
+# Author: Negar Amiri
+# Date: October 16th, 2024
+# Auburn University's "Hey Day" tradition
+
+# Ask the user for their name
+echo "What's your name?"
+read user_name
+
+# Greet the user
+echo "Hey, $user_name! Welcome to Auburn's Hey Day! Hope you're having an awesome day!"
+echo "Make someone's day by saying 'Hey!' today!"
